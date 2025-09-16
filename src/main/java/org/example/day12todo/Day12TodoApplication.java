a00b53d9a4f9d8342bcf84dfadbb36a0210350e3
Felix Linghu <felix.linghu@oocl.com>
2025-09-16 10:35:23 +0800

    init

---
+++ src/main/java/org/example/day12todo/Day12TodoApplication.java
@@ -0,0 +1,13 @@
+package org.example.day12todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12TodoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Day12TodoApplication.class, args);
+  }
+
+}
